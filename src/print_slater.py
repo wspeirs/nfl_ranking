8edd98058f9f2ee383aa474a24b235af06531189
William Speirs <bill.speirs@gmail.com>
2018-02-20 15:17:09 -0500

    Update to paper

---
+++ src/print_slater.py
@@ -0,0 +1,24 @@
+
+rankings = {
+    2002: ['PHI', 'TAM', 'GB', 'SFO', 'OAK', 'TEN', 'ATL', 'NYG', 'PIT', 'IND', 'CAR', 'CLE', 'NWE', 'BUF', 'MIA', 'BAL', 'DEN', 'JAX', 'WAS', 'SEA', 'KC', 'RAM', 'NYJ', 'CHR', 'CIN', 'ARI', 'MIN', 'DET', 'NOR', 'CHI', 'HOU', 'DAL'],
+    2003: ['NWE', 'KC', 'IND', 'TEN', 'PHI', 'MIA', 'RAM', 'BAL', 'CAR', 'GB', 'SEA', 'NOR', 'DEN', 'DAL', 'CHI', 'BUF', 'NYJ', 'PIT', 'CLE', 'OAK', 'CIN', 'JAX', 'CHR', 'MIN', 'SFO', 'TAM', 'HOU', 'WAS', 'ATL', 'NYG', 'DET', 'ARI'] ,
+    2004: ['PIT', 'NWE', 'PHI', 'IND', 'CHR', 'NYJ', 'GB', 'BUF', 'SEA', 'ATL', 'MIN', 'HOU', 'JAX', 'CIN', 'DEN', 'CAR', 'NOR', 'KC', 'OAK', 'BAL', 'DAL', 'WAS', 'DET', 'RAM', 'TAM', 'CHI', 'TEN', 'SFO', 'ARI', 'MIA', 'NYG', 'CLE'],
+    2005: ['IND', 'DEN', 'JAX', 'SEA', 'PIT', 'CIN', 'CHI', 'TAM', 'CAR', 'ATL', 'MIN', 'NYG', 'WAS', 'DAL', 'PHI', 'KC', 'NWE', 'DET', 'CLE', 'MIA', 'CHR', 'ARI', 'OAK', 'TEN', 'BAL', 'SFO', 'GB', 'RAM', 'NOR', 'NYJ', 'BUF', 'HOU'],
+    2006: ['CHR', 'CHI', 'BAL', 'IND', 'NWE', 'NYJ', 'NOR', 'PHI', 'DAL', 'TEN', 'BUF', 'GB', 'NYG', 'HOU', 'ATL', 'DEN', 'CIN', 'PIT', 'KC', 'SFO', 'SEA', 'RAM', 'MIN', 'CAR', 'TAM', 'WAS', 'JAX', 'MIA', 'CLE', 'OAK', 'ARI', 'DET'],
+    2007: ['NWE', 'IND', 'DAL', 'GB', 'JAX', 'CHR', 'NYG', 'PIT', 'CLE', 'SEA', 'PHI', 'BUF', 'TEN', 'HOU', 'TAM', 'WAS', 'DET', 'MIN', 'CHI', 'CAR', 'NOR', 'DEN', 'CIN', 'BAL', 'NYJ', 'SFO', 'KC', 'OAK', 'ARI', 'RAM', 'ATL', 'MIA'],
+    2008: ['TEN', 'PIT', 'BAL', 'CAR', 'MIA', 'NWE', 'ATL', 'NYG', 'DAL', 'IND', 'CHR', 'WAS', 'MIN', 'ARI', 'HOU', 'DEN', 'TAM', 'CHI', 'NOR', 'PHI', 'SFO', 'NYJ', 'CIN', 'CLE', 'BUF', 'KC', 'OAK', 'JAX', 'GB', 'SEA', 'RAM', 'DET'],
+    2009: ['IND', 'CHR', 'NOR', 'PHI', 'MIN', 'GB', 'NWE', 'ATL', 'SFO', 'ARI', 'CIN', 'BAL', 'CHI', 'NYG', 'DAL', 'JAX', 'HOU', 'PIT', 'MIA', 'NYJ', 'TEN', 'SEA', 'DET', 'CLE', 'BUF', 'CAR', 'KC', 'WAS', 'DEN', 'OAK', 'RAM', 'TAM'],
+    2010: ['NWE', 'ATL', 'BAL', 'PIT', 'TAM', 'CHI', 'NYJ', 'NOR', 'GB', 'PHI', 'IND', 'NYG', 'KC', 'MIN', 'JAX', 'BUF', 'DET', 'RAM', 'SFO', 'CLE', 'MIA', 'CIN', 'OAK', 'CHR', 'SEA', 'CAR', 'ARI', 'DEN', 'TEN', 'DAL', 'HOU', 'WAS'],
+    2011: ['GB', 'NOR', 'NWE', 'SFO', 'DET', 'PIT', 'ATL', 'BAL', 'CIN', 'TEN', 'NYG', 'ARI', 'PHI', 'DAL', 'NYJ', 'CAR', 'HOU', 'CHR', 'MIA', 'BUF', 'OAK', 'KC', 'DEN', 'CHI', 'CLE', 'JAX', 'TAM', 'MIN', 'WAS', 'SEA', 'RAM', 'IND'],
+    2012: ['ATL', 'DEN', 'SFO', 'NWE', 'HOU', 'IND', 'GB', 'CHI', 'WAS', 'BAL', 'DAL', 'CIN', 'MIN', 'TEN', 'NYG', 'NOR', 'TAM', 'CHR', 'DET', 'MIA', 'SEA', 'PIT', 'NYJ', 'RAM', 'BUF', 'ARI', 'CAR', 'PHI', 'CLE', 'OAK', 'KC', 'JAX'],
+    2013: ['SEA', 'DEN', 'CAR', 'SFO', 'NWE', 'KC', 'NOR', 'PHI', 'ARI', 'CIN', 'IND', 'TEN', 'GB', 'DAL', 'BAL', 'NYJ', 'RAM', 'TAM', 'NYG', 'DET', 'OAK', 'MIA', 'ATL', 'MIN', 'WAS', 'CHI', 'PIT', 'JAX', 'HOU', 'CLE', 'CHR', 'BUF'],
+    2014: ['DAL', 'SEA', 'GB', 'NWE', 'DEN', 'IND', 'ARI', 'DET', 'PHI', 'HOU', 'BAL', 'PIT', 'CIN', 'KC', 'CHR', 'MIA', 'BUF', 'MIN', 'CAR', 'CLE', 'CHI', 'NYJ', 'NYG', 'ATL', 'RAM', 'OAK', 'SFO', 'NOR', 'TAM', 'WAS', 'TEN', 'JAX'],
+    2015: ['CAR', 'ARI', 'CIN', 'NWE', 'MIN', 'KC', 'DEN', 'GB', 'SEA', 'PIT', 'DET', 'OAK', 'NYJ', 'WAS', 'HOU', 'NOR', 'IND', 'CHI', 'TAM', 'ATL', 'CHR', 'JAX', 'PHI', 'NYG', 'BUF', 'DAL', 'MIA', 'BAL', 'RAM', 'CLE', 'SFO', 'TEN'],
+    2016: ['NWE', 'DAL', 'PIT', 'KC', 'OAK', 'ATL', 'DEN', 'GB', 'NYG', 'DET', 'MIA', 'NOR', 'TAM', 'SEA', 'WAS', 'BAL', 'BUF', 'HOU', 'CIN', 'PHI', 'IND', 'MIN', 'CAR', 'ARI', 'TEN', 'NYJ', 'CLE', 'CHR', 'JAX', 'CHI', 'SFO', 'RAM'],
+    2017: ['PHI', 'NWE', 'PIT', 'MIN', 'RAM', 'NOR', 'CAR', 'BUF', 'ATL', 'DET', 'GB', 'DAL', 'ARI', 'TAM', 'KC', 'JAX', 'CHR', 'WAS', 'SEA', 'SFO', 'CHI', 'BAL', 'OAK', 'NYG', 'MIA', 'TEN', 'CIN', 'DEN', 'IND', 'HOU', 'NYJ', 'CLE']
+}
+
+print(*list(range(2002, 2018)), sep=" & ")
+for i in range(1, 33):
+    print("{} & ".format(i), end='')
+    print(*[rankings[year][i-1] for year in range(2002, 2018)], sep=" & ")
